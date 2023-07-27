e6ab5ecb1b6f4315b0d8108ad254c60e3b3e28b0
Ankit Kumar <developer.xda2@gmail.com>
2023-07-27 09:50:00 +0530

    Leap Year

--- Sum.java
+++ Sum.java
@@ -1,7 +1,8 @@
-class Sum{
+public class Sum {
     public static void main(String args[]){
-        int a = 2 , b = 4;
+        int a  =2 , b =4;
         int sum = a+b;
         System.out.println(sum);
     }
+    
 }
